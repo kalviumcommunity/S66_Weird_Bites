3118b618094444867352194eea455a458048c082
shebin <mohammedshebinc92@gmail.com>
2025-02-06 16:27:58 +0530

    CRUD added

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const app=express()
 require('dotenv').config()
 const mongoose=require('mongoose')
 const UserModel =require("./Models/users")
+const router=require("./routes/router")
 
-let dbConnection="Disconnected"
+let dbConnection="Disconnected" 
 
 app.use(express.json());
 
@@ -25,35 +26,23 @@ app.get("/",(req,res)=> {
     }
 })
 
-
-app.get('/ping',(req,res) =>{
-    res.send('pong')
-})
-
-app.post("/create",async(req,res)=> {
-    const{username,password,email}=req.body
-    payload={username,password,email}
-
-    try {
-        let newUser=UserModel(payload)
-        await newUser.save()
-        res.send({"mesaage":"Hurray! Saved Successfully.."})
-    } catch (error) {
-        console.log(error)
-        res.send({error:"error"})
-    }
-})
+app.use("/api",router)
 
 
 app.listen(process.env.PORT,async()=>{
-    // try{
-    //     // await connection;
-    //     dbConnection="Connected"
-    //     console.log("Successfully connected to mongoDb")
-    // }
-    // catch(error){
-    //     console.log(error)
-    //     dbConnection=`Error: ${error.mesaage}`
-    // }
     console.log(`Server is running on port ${process.env.PORT}`)
 })
+
+// app.post("/create",async(req,res)=> {
+//     const{username,password,email}=req.body
+//     payload={username,password,email}
+
+//     try {
+//         let newUser=UserModel(payload)
+//         await newUser.save()
+//         res.send({"mesaage":"Hurray! Saved Successfully.."})
+//     } catch (error) {
+//         console.log(error)
+//         res.send({error:"error"})
+//     }
+// })
